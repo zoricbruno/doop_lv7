695a8e3685c169f9e29426bcff2828004718d002
Bruno <bruno.ferit@gmail.com>
2021-05-10 13:11:33 +0200

    Added Fizzbuzzer and tests.

---
+++ LV7/FizzBuzz.Test/FizzbuzzerTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz.Test
+{ 
+    [TestFixture]
+    class FizzbuzzerTests
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void Convert_WhenInputIsIndivisible_ReturnsNumberAsString(int number)
+        {
+            // Arrange
+            FizzBuzzer fizzBuzzer = new FizzBuzzer();
+            string expected = number.ToString();
+
+            // Act
+            string actual = fizzBuzzer.Convert(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void Convert_WhenInputIsDivisibleByFizz_ReturnsFizz(int number)
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            string expected = "Fizz";
+
+            string actual = fizzBuzzer.Convert(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void Convert_WhenInputIsDivisibleByBuzz_ReturnsBuzz(int number)
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            string expected = "Buzz";
+
+            string actual = fizzBuzzer.Convert(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void Convert_WhenInputIsDivisibleByFizzAndBuzz_ReturnsFizzBuzz(int number)
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            string expected = "FizzBuzz";
+
+            string actual = fizzBuzzer.Convert(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ LV7/FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Convert(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            if (number % 3 == 0)
+                return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
+            return number.ToString();
+        }
+    }
+}
